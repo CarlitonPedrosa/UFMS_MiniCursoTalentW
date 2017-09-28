2d46803183a083f1d9a187b1341807e42a98052e
Carliton Pedrosa Pessoa <carlitonpedrosa@gmail.com>
2017-09-28 19:11:20 -0400

    aula 05

---
+++ MiniCurso - TalentW/JS/calculadora.js
@@ -0,0 +1,42 @@
+var x, y;
+var sinal;
+
+function add1(){
+    document.getElementById("visor").value += 1;
+}
+
+function ada2(){
+    document.getElementById("visor").value += 2;
+}
+
+function add3(){
+    document.getElementById("visor").value += 3;
+}
+
+function add4(){
+    document.getElementById("visor").value += 4;
+}
+
+function add5(){
+    document.getElementById("visor").value += 5;
+}
+
+function add6(){
+    document.getElementById("visor").value += 6;
+}
+
+function add7(){
+    document.getElementById("visor").value += 7;
+}
+
+function add8(){
+    document.getElementById("visor").value += 8;
+}
+
+function add9(){
+    document.getElementById("visor").value += 9;
+}
+
+function add0(){
+    document.getElementById("visor").value += 0;
+}
